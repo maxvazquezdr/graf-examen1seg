2b6d104fe4c1bddb0e314d64da6d5a91ed2334f6
maxvazquezdr <maxvazquezdr@gmail.com>
2026-09-23 09:50:18 -0600

    Ajustes SVG + Control de dimensión

---
+++ graf-ejercicio1s/script.js
@@ -0,0 +1,39 @@
+// Obtener la imagen y los botones
+const image = document.getElementById('image');
+const xSmallBtn = document.getElementById('xSmallBtn');
+const smallBtn = document.getElementById('smallBtn');
+const mediumBtn = document.getElementById('mediumBtn');
+const largeBtn = document.getElementById('largeBtn');
+const xLargeBtn = document.getElementById('xLargeBtn');
+
+// Funciones para cambiar el tamaño de la imagen
+
+// X-Pequeño!
+xSmallBtn.addEventListener('click', () => {
+    image.style.width = '75px';
+    image.style.height = '75px';
+});
+
+// Pequeño
+smallBtn.addEventListener('click', () => {
+    image.style.width = '150px';
+    image.style.height = '150px';
+});
+
+// Mediano
+mediumBtn.addEventListener('click', () => {
+    image.style.width = '300px';
+    image.style.height = '300px';
+});
+
+// Grande
+largeBtn.addEventListener('click', () => {
+    image.style.width = '450px';
+    image.style.height = '450px';
+});
+
+// X-Grande!
+xLargeBtn.addEventListener('click', () => {
+    image.style.width = '600px';
+    image.style.height = '600px';
+});
